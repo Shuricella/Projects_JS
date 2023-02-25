ef978a84413cc155a889dfd226d6923fa4de00fd
Shuricella <Shuricella@gmail.com>
2023-02-25 21:09:50 +0200

    commit(25/02/23)_21-10

---
+++ Online_Store_grid_(JS)/filters_list.js
@@ -0,0 +1,189 @@
+export default class FiltersList {
+    constructor() {
+        this.render();
+    }
+
+    getTemplate() {
+        return `
+            <section>
+
+                <div class="wrapper-filters-list">
+
+                    <section class="block-slider">
+                        <h2 class="block-slider-text">Prise</h2>
+                
+                        <section class="wrapper-slider">
+                            <input type="range" class="form-range-slider" min="0" max="85000"  step="1000" id="customRange1">
+                    
+                            <div class="block-slider-price">
+                                <div class="slider-price"><p class="slider-number">0 UA</p></div>
+                                <div class="slider-price"><p class="slider-number">85000 UA</p></div>
+                            </div>
+                        </section>
+                    </section>
+            
+            
+                    <section class="category-filters-list">
+            
+                        <h2 class="block-category-brand">Category</h2>
+                
+                        <form action="#">
+                
+                            <ul class="blocks-cat-list">
+                            
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-monitors" class="cursor-filter-list" name="category" value="category=monitors">
+                                    <label for="blocks-category-monitors" class="cursor-filter-list none-style-filters text-color-filters">Monitors</label>
+                                </li>
+                                
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-laptops" class="cursor-filter-list" name="category" value="category=laptops">
+                                    <label for="blocks-category-laptops" class="cursor-filter-list none-style-filters text-color-filters">LapTops</label>
+                                </li>
+                    
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-video-cards" class="cursor-filter-list" name="category" value="category=video_cards">
+                                    <label for="blocks-category-video-cards" class="cursor-filter-list none-style-filters text-color-filters">Video cards</label>
+                                </li>
+                    
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-gaming-keyboards" class="cursor-filter-list" name="category" value="category=gaming_keyboards">
+                                    <label for="blocks-category-gaming-keyboards" class="cursor-filter-list none-style-filters text-color-filters">Gaming keyboards</label>
+                                </li>
+                    
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-computer-mouse" class="cursor-filter-list" name="category" value="category=computer_mouse">
+                                    <label for="blocks-category-computer-mouse" class="cursor-filter-list none-style-filters text-color-filters">Computer mouse</label>
+                                </li>
+                        
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-ssd" class="cursor-filter-list" name="category" value="category=ssd">
+                                    <label for="blocks-category-ssd" class="cursor-filter-list none-style-filters text-color-filters">SSD</label>
+                                </li>
+                        
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-sound-cards" class="cursor-filter-list" name="category" value="category=sound_cards">
+                                    <label for="blocks-category-sound-cards" class="cursor-filter-list none-style-filters text-color-filters">Sound cards</label>
+                                </li>
+                        
+                                <li class="blocks-cat-br">
+                                    <input type="checkbox" id="blocks-category-ram" class="cursor-filter-list" name="category" value="category=ram">
+                                    <label for="blocks-category-ram" class="cursor-filter-list none-style-filters text-color-filters">RAM</label>
+                                </li>
+                            </ul>
+                
+                        </form>
+            
+                    </section>
+            
+            
+                    <section class="brand">
+            
+                        <h2 class="block-category-brand strip">Brand</h2>
+                
+                        <form action="#">
+                
+                            <ul class="blocks-cat-list">
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-asus" class="cursor-filter-list" name="brand" value="brand=asus">
+                                <label for="blocks-brand-asus" class="cursor-filter-list none-style-filters text-color-filters">Asus</label>
+                            </li>
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-acer" class="cursor-filter-list" name="brand" value="brand=acer">
+                                <label for="blocks-brand-acer" class="cursor-filter-list none-style-filters text-color-filters">Acer</label>
+                            </li>
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-apple" class="cursor-filter-list" name="brand" value="brand=apple">
+                                <label for="blocks-brand-apple" class="cursor-filter-list none-style-filters text-color-filters">Apple</label>
+                            </li>
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-dell" class="cursor-filter-list" name="brand" value="brand=dell">
+                                <label for="blocks-brand-dell" class="cursor-filter-list none-style-filters text-color-filters">Dell</label>
+                            </li>
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-dynamode" class="cursor-filter-list" name="brand" value="brand=dynamode">
+                                <label for="blocks-brand-dynamode" class="cursor-filter-list none-style-filters text-color-filters">Dynamode</label>
+                            </li>
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-gigabyte" class="cursor-filter-list" name="brand" value="brand=gigabyte">
+                                <label for="blocks-brand-gigabyte" class="cursor-filter-list none-style-filters text-color-filters">Gigabyte</label>
+                            </li>
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-kingston" class="cursor-filter-list" name="brand" value="brand=kingston">
+                                <label for="blocks-brand-kingston" class="cursor-filter-list none-style-filters text-color-filters">Kingston</label>
+                            </li>
+                
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-lenovo" class="cursor-filter-list" name="brand" value="brand=lenovo">
+                                <label for="blocks-brand-lenovo" class="cursor-filter-list none-style-filters text-color-filters">Lenovo</label>
+                            </li>
+                    
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-logitech" class="cursor-filter-list" name="brand" value="brand=logitech">
+                                <label for="blocks-brand-logitech" class="cursor-filter-list none-style-filters text-color-filters">Logitech</label>
+                            </li>
+                    
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-msi" class="cursor-filter-list" name="brand" value="brand=msi">
+                                <label for="blocks-brand-msi" class="cursor-filter-list none-style-filters text-color-filters">MSI</label>
+                            </li>
+                    
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-benq" class="cursor-filter-list" name="brand" value="brand=benq">
+                                <label for="blocks-brand-benq" class="cursor-filter-list none-style-filters text-color-filters">BenQ</label>
+                            </li>
+                    
+                            <li class="blocks-cat-br">
+                                <input type="checkbox" id="blocks-brand-a4tech" class="cursor-filter-list" name="brand" value="brand=a4tech">
+                                <label for="blocks-brand-a4tech" class="cursor-filter-list none-style-filters text-color-filters">A4Tech</label>
+                            </li>
+                
+                            </ul>
+                
+                        </form>
+            
+                        <div class="block-line"></div>
+            
+                    </section>
+            
+            
+                    <div class="block-slider-rating">
+                        <h2 class="block-slider-text">Rating</h2>
+                
+                        <section class="wrapper-slider">
+                            <input type="range" class="form-range-slider" min="0" max="5"  step="1" id="customRange1">
+                    
+                            <div class="block-slider-price">
+                                <div class="slider-rating"><p class="slider-number">0</p></div>
+                                <div class="slider-rating"><p class="slider-number">5</p></div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+                                                                
+                <section class="wrapper-butto-clear">
+                    <button type="button" class="butto-clear">CLEAR ALL FILTERS</button>
+                </section>
+
+            </section>
+        `;
+    }
+
+    render() {
+        const wrapper = document.createElement("div");
+
+        wrapper.innerHTML = this.getTemplate();
+
+        // помещаем элемент в наш обьект
+        this.element = wrapper.firstElementChild;
+    }
+}
+
+ 
---
+++ Online_Store_grid_(JS)/index.js
@@ -0,0 +1,128 @@
+// В этой компоненте будет вся логика от всей страницы и другие компоненты
+import CardsList from "./cards-list.js";
+import Pagination from "./pagination.js";
+import FiltersList from "./filters_list.js";
+
+// products?_page=1&_limit=9
+const backendUrl = `https://online-store.bootcamp.place/api/`;
+
+export default class OnlineStorePage {
+    constructor() {
+        // Колличество страничек на листе
+        this.pageSize = 9;
+
+        // указываем, что products = [] по умолчанию пустой массив
+        this.products = [];
+
+        // добавляем параметр "products" к адресу "beckendUrl"
+        // необходимо поменять this.url на this.productsurl
+        // будет еще this.Categoryurl и this.Brandurl
+        this.url = new URL("products", backendUrl);
+        this.url.searchParams.set("_limit", this.pageSize);
+        
+        this.components = {};
+        
+        this.initComponents();
+        this.render();
+        this.renderComponents();
+
+        this.initEventListeners();
+
+        this.update(1);
+    }
+
+    async loadData(pageNumber) {
+        // этот метод отвечает за загрузку данных
+
+        this.url.searchParams.set("_page", pageNumber);
+
+        const response = await fetch(this.url);
+        const products = await response.json();
+
+        console.log("products=", products);
+
+        // fetch(url)
+        // .then(response => response.json())
+        // .then(products => {
+        //     console.log("products=", products);
+        // });
+
+        return products;
+    }
+
+    getTemplate() {
+        return `
+        <div class="wrapper-grid">
+            <aside class="item-products" data-element="filtersList">
+                <!-- FiltersList -->
+            </aside>
+
+            <div class="item-main" data-element="cardsList">
+                <!-- Card List component -->
+            </div>
+
+            <div class="item-pagination" data-element="pagination">
+                <!--Pagination component-->
+            </div>
+        </div>
+        `;
+    }
+
+    initComponents() {
+        // Зададим колличиство элементов в объекте products
+        const totalElements = 100;
+        const totalPages = Math.ceil(totalElements / this.pageSize);
+
+        // const cardsList = new CardsList(this.products.slice(0, this.pageSize));
+        
+        const cardsList = new CardsList(this.products);
+        const pagination = new Pagination({
+            activePageIndex: 0,
+            totalPages: totalPages
+        });
+        const filtersList = new FiltersList();
+
+        this.components.cardsList = cardsList;
+        this.components.pagination = pagination;
+        this.components.filtersList = filtersList;
+    }
+
+    renderComponents() {
+        const cardsConteiner = this.element.querySelector('[data-element="cardsList"]');
+        const paginationConteiner = this.element.querySelector('[data-element="pagination"]');
+        const filtersListConteiner = this.element.querySelector('[data-element="filtersList"]');
+        console.log("filtersListConteiner=", filtersListConteiner);
+
+        cardsConteiner.append(this.components.cardsList.element);
+        paginationConteiner.append(this.components.pagination.element);
+        filtersListConteiner.append(this.components.filtersList.element);
+    }
+
+    render() {
+        const wrapper = document.createElement("div");
+
+        wrapper.innerHTML = this.getTemplate();
+
+        // помещаем элемент в наш обьект
+        this.element = wrapper.firstElementChild;
+    }
+
+    initEventListeners() {
+        this.components.pagination.element.addEventListener("page-changed", event => {
+            const pageIndex = event.detail;
+
+            this.update(pageIndex + 1);
+        })
+    }
+
+    async update(pageNumber) {
+        // этот метод реализует логику отрисовки
+
+        // const start = this.pageSize * pageIndex;
+        // const end = start + this.pageSize;
+        // const data = this.products.slice(start, end);
+        const data = await this.loadData(pageNumber);
+
+        this.components.cardsList.update(data);
+    }
+}
